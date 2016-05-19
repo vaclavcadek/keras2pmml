c9dc0a5b1929b779d022dd2e7ff05b99e8520dc6
Václav Čadek <vaclavcadek@gmail.com>
2016-05-19 16:02:39 +0200

    XML generation refactored to use etree

--- keras2pmml/__init__.py
+++ keras2pmml/__init__.py
@@ -1,5 +1,8 @@
-from xml.dom.minidom import Document
-
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+from datetime import datetime
 from keras.models import Sequential
 from sklearn.preprocessing import StandardScaler
 
@@ -12,7 +15,7 @@ SUPPORTED_ACTIVATIONS = {
 
 
 def _validate_inputs(model, transformer, feature_names, target_values):
-    print('[X] Performing model validation.')
+    print('[x] Performing model validation.')
     if not type(model) in SUPPORTED_MODELS:
         raise TypeError("Provided model is not supported.")
     if not model.built:
@@ -25,123 +28,129 @@ def _validate_inputs(model, transformer, feature_names, target_values):
     if model.output_shape[1] != len(target_values):
         print('[!] Network output shape does not match provided target values - using generic names instead.')
         target_values = ['y{}'.format(i) for i in range(model.output_shape[1])]
-    print('[X] Model validation successful.')
+    print('[x] Model validation successful.')
     return feature_names, target_values
 
 
-def _generate_metadata(doc, root, kwargs):
+def _generate_header(root, kwargs):
     description = kwargs.get('description', None)
-    header = doc.createElement('Header')
-    root.appendChild(header)
-    header.attributes['copyright'] = kwargs.get('copyright', '')
-    header.attributes['description'] = description
-    timestamp = doc.createElement('Timestamp')
-    header.appendChild(timestamp)
+    header = ET.SubElement(root, 'Header')
+    header.set('copyright', kwargs.get('copyright', ''))
+    header.set('description', description)
+    timestamp = ET.SubElement(header, 'Timestamp')
+    timestamp.text = str(datetime.now())
+    return header
 
 
-def _generate_variables(doc, root, neural_network, transformer, feature_names, target_name, target_values):
-    data_dict = doc.createElement('DataDictionary')
-    root.appendChild(data_dict)
-    data_field = doc.createElement('DataField')
-    data_dict.appendChild(data_field)
-    data_field.attributes['name'] = target_name
-    data_field.attributes['dataType'] = 'string'
-    data_field.attributes['optype'] = 'categorical'
-    mining_schema = doc.createElement('MiningSchema')
-    neural_network.appendChild(mining_schema)
-    mining_field = doc.createElement('MiningField')
-    mining_schema.appendChild(mining_field)
-    mining_field.attributes['name'] = target_name
-    mining_field.attributes['usageType'] = 'target'
-    output = doc.createElement('Output')
+def _generate_data_dictionary(root, feature_names, target_name, target_values):
+    data_dict = ET.SubElement(root, 'DataDictionary')
+    data_field = ET.SubElement(data_dict, 'DataField')
+    data_field.set('name', target_name)
+    data_field.set('dataType', 'string')
+    data_field.set('optype', 'categorical')
+    print('[x] Generating Data Dictionary:')
     for t in target_values:
-        value_field = doc.createElement('Value')
-        data_field.appendChild(value_field)
-        value_field.attributes['value'] = t
+        value = ET.SubElement(data_field, 'Value')
+        value.set('value', t)
+    for f in feature_names:
+        data_field = ET.SubElement(data_dict, 'DataField')
+        data_field.set('name', f)
+        data_field.set('dataType', 'double')
+        data_field.set('optype', 'continuous')
+        print('\t[-] {}...OK!'.format(f))
+    return data_dict
 
-        output_field = doc.createElement('OutputField')
-        output.appendChild(output_field)
-        output_field.attributes['name'] = 'probability_{}'.format(t)
-        output_field.attributes['feature'] = 'probability'
-        output_field.attributes['value'] = t
-    neural_network.appendChild(output)
-    neural_inputs = doc.createElement('NeuralInputs')
-    neural_network.appendChild(neural_inputs)
+
+def _generate_neural_network(root, estimator, transformer, feature_names, target_name, target_values, model_name=None):
+    neural_network = ET.SubElement(root, 'NeuralNetwork')
+    neural_network.set('functionName', 'classification')
+    if model_name:
+        neural_network.set('modelName', model_name)
+    _generate_mining_schema(neural_network, feature_names, target_name)
+    _generate_output(neural_network, target_values)
+    _generate_neural_inputs(neural_network, transformer, feature_names)
+    _generate_neural_layers(neural_network, estimator)
+    _generate_neural_outputs(neural_network, estimator, target_name, target_values)
+    return neural_network
+
+
+def _generate_mining_schema(neural_network, feature_names, target_name):
+    mining_schema = ET.SubElement(neural_network, 'MiningSchema')
+    mining_field = ET.SubElement(mining_schema, 'MiningField')
+    mining_field.set('name', target_name)
+    mining_field.set('usageType', 'target')
+    for f in feature_names:
+        mining_field = ET.SubElement(mining_schema, 'MiningField')
+        mining_field.set('name', f)
+    return mining_schema
+
+
+def _generate_output(neural_network, target_values):
+    output = ET.SubElement(neural_network, 'Output')
+    for t in target_values:
+        output_field = ET.SubElement(output, 'OutputField')
+        output_field.set('name', 'probability_{}'.format(t))
+        output_field.set('feature', 'probability')
+        output_field.set('value', t)
+    return output
+
+
+def _generate_neural_inputs(neural_network, transformer, feature_names):
+    neural_inputs = ET.SubElement(neural_network, 'NeuralInputs')
     for i, f in enumerate(feature_names):
-        data_field = doc.createElement('DataField')
-        data_dict.appendChild(data_field)
-        data_field.attributes['name'] = f
-        data_field.attributes['dataType'] = 'double'
-        data_field.attributes['optype'] = 'continuous'
-
-        mining_field = doc.createElement('MiningField')
-        mining_schema.appendChild(mining_field)
-        mining_field.attributes['name'] = f
-
-        neural_input = doc.createElement('NeuralInput')
-        neural_inputs.appendChild(neural_input)
-        neural_input.attributes['id'] = '0,{}'.format(i)
-        derived_field = doc.createElement('DerivedField')
-        neural_input.appendChild(derived_field)
-        derived_field.attributes['optype'] = 'continuous'
-        derived_field.attributes['dataType'] = 'double'
+        neural_input = ET.SubElement(neural_inputs, 'NeuralInput')
+        neural_input.set('id', '0,{}'.format(i))
+        derived_field = ET.SubElement(neural_input, 'DerivedField')
+        derived_field.set('optype', 'continuous')
+        derived_field.set('dataType', 'double')
         if transformer is not None:
-            norm_continuous = doc.createElement('NormContinuous')
-            derived_field.appendChild(norm_continuous)
-            norm_continuous.attributes['field'] = f
-            ln1 = doc.createElement('LinearNorm')
-            norm_continuous.appendChild(ln1)
-            ln2 = doc.createElement('LinearNorm')
-            norm_continuous.appendChild(ln2)
-            ln1.attributes['orig'] = '0.0'
-            ln1.attributes['norm'] = str(- transformer.mean_[i] / transformer.scale_[i])
-            ln2.attributes['orig'] = str(transformer.mean_[i])
-            ln2.attributes['norm'] = '0.0'
+            norm_continuous = ET.SubElement(derived_field, 'NormContinuous')
+            norm_continuous.set('field', f)
+            ln1 = ET.SubElement(norm_continuous, 'LinearNorm')
+            ln2 = ET.SubElement(norm_continuous, 'LinearNorm')
+            ln1.set('orig', '0.0')
+            ln1.set('norm', str(- transformer.mean_[i] / transformer.scale_[i]))
+            ln2.set('orig', str(transformer.mean_[i]))
+            ln2.set('norm', '0.0')
         else:
-            field_ref = doc.createElement('FieldRef')
-            derived_field.appendChild(field_ref)
-            field_ref.attributes['field'] = f
+            field_ref = ET.SubElement(derived_field, 'FieldRef')
+            field_ref.set('field', f)
 
 
-def _generate_layers(doc, estimator, neural_network, target_name, target_values):
+def _generate_neural_layers(neural_network, estimator):
     layer_weights = estimator.get_weights()[0::2]
     layer_biases = estimator.get_weights()[1::2]
     layer_activations = map(lambda x: x['config']['activation'], estimator.get_config())
-    last_layer = len(layer_weights) - 1
     for layer, params in enumerate(zip(layer_weights, layer_biases, layer_activations)):
         weights = params[0]
         biases = params[1]
         activation = params[2]
-        neural_layer = doc.createElement('NeuralLayer')
-        neural_network.appendChild(neural_layer)
-        neural_layer.attributes['activationFunction'] = SUPPORTED_ACTIVATIONS[activation]
+        neural_layer = ET.SubElement(neural_network, 'NeuralLayer')
+        neural_layer.set('activationFunction', SUPPORTED_ACTIVATIONS[activation])
         rows = weights.shape[0]
         cols = weights.shape[1]
-        if layer == last_layer:
-            output_layer = doc.createElement('NeuralOutputs')
-            neural_network.appendChild(output_layer)
         for j in range(cols):
-            neuron = doc.createElement('Neuron')
-            neural_layer.appendChild(neuron)
-            neuron.attributes['id'] = '{},{}'.format(layer + 1, j)
-            neuron.attributes['bias'] = str(biases[j])
-            if layer == last_layer:
-                neural_output = doc.createElement('NeuralOutput')
-                output_layer.appendChild(neural_output)
-                neural_output.attributes['outputNeuron'] = '{},{}'.format(layer + 1, j)
-                derived_field = doc.createElement('DerivedField')
-                neural_output.appendChild(derived_field)
-                derived_field.attributes['optype'] = 'continuous'
-                derived_field.attributes['dataType'] = 'double'
-                norm_discrete = doc.createElement('NormDiscrete')
-                derived_field.appendChild(norm_discrete)
-                norm_discrete.attributes['field'] = target_name
-                norm_discrete.attributes['value'] = target_values[j]
+            neuron = ET.SubElement(neural_layer, 'Neuron')
+            neuron.set('id', '{},{}'.format(layer + 1, j))
+            neuron.set('bias', str(biases[j]))
             for i in range(rows):
-                connection = doc.createElement('Con')
-                connection.attributes['from'] = '{},{}'.format(layer, i)
-                connection.attributes['weight'] = str(weights[i, j])
-                neuron.appendChild(connection)
+                connection = ET.SubElement(neuron, 'Con')
+                connection.set('from', '{},{}'.format(layer, i))
+                connection.set('weight', str(weights[i, j]))
+
+
+def _generate_neural_outputs(neural_network, estimator, target_name, target_values):
+    num_layers = estimator.output_shape[1]
+    neural_outputs = ET.SubElement(neural_network, 'NeuralOutputs')
+    for j in range(len(target_values)):
+        neural_output = ET.SubElement(neural_outputs, 'NeuralOutput')
+        neural_output.set('outputNeuron', '{},{}'.format(num_layers, j))
+        derived_field = ET.SubElement(neural_output, 'DerivedField')
+        derived_field.set('optype', 'continuous')
+        derived_field.set('dataType', 'double')
+        norm_discrete = ET.SubElement(derived_field, 'NormDiscrete')
+        norm_discrete.set('field', target_name)
+        norm_discrete.set('value', target_values[j])
 
 
 def keras2pmml(estimator, transformer, file, **kwargs):
@@ -152,19 +161,13 @@ def keras2pmml(estimator, transformer, file, **kwargs):
 
     feature_names, target_values = _validate_inputs(estimator, transformer, feature_names, target_values)
 
-    doc = Document()
-    root = doc.createElement('PMML')
-    root.attributes['xmlns'] = 'http://www.dmg.org/PMML-4_2'
-    root.attributes['version'] = '4.2.1'
-    doc.appendChild(root)
-    _generate_metadata(doc, root, kwargs)
+    pmml = ET.Element('PMML')
+    pmml.set('version', '4.2.1')
+    pmml.set('xmlns', 'http://www.dmg.org/PMML-4_2')
+    _generate_header(pmml, kwargs)
+    _generate_data_dictionary(pmml, feature_names, target_name, target_values)
+    _generate_neural_network(pmml, estimator, transformer, feature_names, target_name, target_values, model_name)
 
-    neural_network = doc.createElement('NeuralNetwork')
-    _generate_variables(doc, root, neural_network, transformer, feature_names, target_name, target_values)
-    root.appendChild(neural_network)
-    neural_network.attributes['modelName'] = model_name
-    neural_network.attributes['functionName'] = 'classification'
-
-    _generate_layers(doc, estimator, neural_network, target_name, target_values)
-
-    doc.writexml(open(file, 'w'), indent=' ', addindent=' ', newl='\n')
+    tree = ET.ElementTree(pmml)
+    tree.write(file)
+    print('[x] Generation of PMML successful.')
