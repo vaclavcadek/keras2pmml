188143db9c4481b8cf429e81bcb55fb4bef367ae
Václav Čadek <vaclavcadek@gmail.com>
2016-12-21 16:36:36 +0100

    Added dependencies to circle.yml to satisfy numpy and scipy pre-requisites

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ setup(
     url='https://github.com/vaclavcadek/keras2pmml',
     author='Václav Čadek',
     author_email='vaclavcadek@gmail.com',
-    setup_requires=['numpy>=1.6.1', 'SciPy >= 0.9'],
-    install_requires=['theano==0.8.2', 'keras==1.0.6', 'scikit-learn==0.17.1'],
+    install_requires=['numpy>=1.6.1', 'SciPy >= 0.9', 'theano==0.8.2', 'keras==1.0.6', 'scikit-learn==0.17.1'],
     classifiers=[
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
